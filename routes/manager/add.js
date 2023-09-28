c293de14d0db5af2f1a61f42c6039431a95886bd
AchiniSNA <119773726+AchiniSNA@users.noreply.github.com>
2023-09-28 21:44:46 +0530

    Update add.js

--- routes/manager/add.js
+++ routes/manager/add.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const connection = require('../../connection');
+const router = express.Router();
 
+router.post('/',function(req,res){
+    
+    var productName = req.body.product_name;
+    var unitPrice = req.body.unit_price;
+    var unitCapacity = req.body.unit_capacity;
+    var stockAmount = req.body.stock_amount;
+    var lastRefilledDate = req.body.last_refilled_date;
+    var availabilityStatus = req.body.availability_status;
+    
+
+    query = "insert into product(product_ID,product_name,unit_price,unit_capacity,stock_amount,last_refilled_date,availability_status) values(?,?,?,?,?,?,?) ";
+    connection.query(query,[13,productName,unitPrice,unitCapacity,stockAmount,lastRefilledDate,availabilityStatus],(err,results)=>{
+        if(!err){
+                res.render("manager/addSucceed.ejs")          
+        }
+
+        else{
+            return res.status(500).json(err);
+        }
+    })
+
+});
+
+
+module.exports = router;
